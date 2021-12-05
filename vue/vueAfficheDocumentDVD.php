2cacc5fce7f99e85206271a279ae4628d0aa26a5
Chromalgame <mattbubu@free.fr>
2021-12-05 22:21:29 +0100

    init

--- vue/vueAfficheDocumentDVD.php
+++ vue/vueAfficheDocumentDVD.php
@@ -1,42 +1,27 @@
 ﻿<label for="search_document">
-	Rechercher un document dans la liste : 
+	Rechercher un document dans la liste :
 </label>
 <input onkeyup="docsearch()" class="search_document" type="search" id="search_document">
 <hr>
-<div class="dvd" data-id="" data-titre="Test">
-	<h2>DVD : </h2>
-	<u><b>ID :</b></u>
-	<div class="Document_dvd_id"></div>
-	<u><b>Titre : </b></u>
-	<div class="Document_dvd_titre">Test</div>
-	<u><b>Synopsis : </b></u>
-	<div class="Document_dvd_synopsis"></div>
-	<u><b>ISBN : </b></u>
-	<div class="Document_dvd_isbn"></div>
-	<u><b>Auteur : </b></u>
-	<div class="Document_dvd_auteur"></div>
-	<u><b>Genre : </b></u>
-	<div class="Document_dvd_genre"></div>
-	<u><b>Public : </b></u>
-	<div class="Document_dvd_public"></div>
-	<hr>
-</div>
-
-<div class="dvd" data-id="" data-titre="Matt">
-	<h2>DVD : </h2>
-	<u><b>ID :</b></u>
-	<div class="Document_dvd_id"></div>
-	<u><b>Titre : </b></u>
-	<div class="Document_dvd_titre">Matt</div>
-	<u><b>Synopsis : </b></u>
-	<div class="Document_dvd_synopsis"></div>
-	<u><b>ISBN : </b></u>
-	<div class="Document_dvd_isbn"></div>
-	<u><b>Auteur : </b></u>
-	<div class="Document_dvd_auteur"></div>
-	<u><b>Genre : </b></u>
-	<div class="Document_dvd_genre"></div>
-	<u><b>Public : </b></u>
-	<div class="Document_dvd_public"></div>
-	<hr>
-</div>
+<?php
+foreach ($listeDVD as $dvd) {
+?>
+	<div class="dvd" data-titre="<?= $dvd->getTitre() ?>">
+		<h2>DVD : </h2>
+		<u><b>ID :</b></u>
+		<div class="Document_dvd_id"><?= $dvd->getId() ?></div>
+		<u><b>Titre : </b></u>
+		<div class="Document_dvd_titre"><?= $dvd->getTitre() ?></div>
+		<u><b>Synopsis : </b></u>
+		<div class="Document_dvd_synopsis"><?= $dvd->getSynopsis() ?></div>
+		<u><b>Realisateur : </b></u>
+		<div class="Document_dvd_realisateur"><?= $dvd->getrealisateur() ?></div>
+		<u><b>Genre : </b></u>
+		<div class="Document_dvd_genre"><?= $dvd->getGenre()->getLibelle() ?></div>
+		<u><b>Public : </b></u>
+		<div class="Document_dvd_public"><?= $dvd->getPublic()->getLibelle() ?></div>
+		<hr>
+	</div>
+<?php
+}
+?>
