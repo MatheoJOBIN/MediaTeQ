1f8f4dfa49f354a90064da14b05b0817f0b33537
Chromalgame <mattbubu@free.fr>
2021-12-05 21:59:58 +0100

    init

--- js/docsearch.js
+++ js/docsearch.js
@@ -1,13 +1,12 @@
 function docsearch() {
     value = document.querySelector("#search_document").value.toLowerCase();
-    titre = document.querySelectorAll("#Document_livre_titre");
-    for (i = 0; i < titre.length; i++) {
-        content = titre[i].textContent || titre[i].innerText;
-        content = content.toLowerCase();
+    livre = document.querySelectorAll(".livre");
+    for (i = 0; i < livre.length; i++) {
+        content = livre[i].dataset.titre.toLowerCase();
         if(content.includes(value)){
-            titre[i].style.display = "block";
+            livre[i].style.display = "block";
         }else{
-            titre[i].style.display = "none";
+            livre[i].style.display = "none";
         }
     }
 }
