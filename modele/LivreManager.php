20cd37301e4f16031879e39bc5db2ac915a50f37
MatheoJOBIN <80678160+MatheoJOBIN@users.noreply.github.com>
2021-12-05 21:57:26 +0100

    LivreManager

--- modele/LivreManager.php
+++ modele/LivreManager.php
@@ -3,14 +3,18 @@
 require_once("modele/Manager.php");
 require_once("modele/Livre.php");
 
-class LivreManager extends Livre
+class LivreManager extends Manager
 {
-    public function get($id) //instancie un objet port
+    public function get($id) //Récupère un objet livre
     {
         $id = (int) $id;
-        $q = $this->getPDO()->query('SELECT id, nom FROM port WHERE id = '.$id);
+        $q = $this->getPDO()->query('SELECT document.idDoc, document.titre, document.synopsis, document.empruntable, livre.ISBN, livre.auteur, document.idGenre, genre.libelle, document.idPublic, public.libelle FROM document JOIN livre ON document.idDoc = livre.idDoc JOIN genre ON document.idGenre = genre.idGenre JOIN public ON document.idPublic = public.idPublic WHERE document.id = '.$id);
+
         $donnees = $q->fetch(PDO::FETCH_ASSOC);
-        return new Port($donnees['id'], $donnees['nom']);
+        $unGenre = new Genre($donnees['idGenre'], $donnees['genre.libelle']);
+        $unPublic = new Publics($donnees['idPublic'], $donnees['public.libelle']);
+        
+        return new Livre($donnees['idDoc'], $donnees['titre'], $donnees['synopsis'], $donnees['empruntable'], $unGenre, $unPublic, $donnees['isbn'], $donnees['auteur']);
     }
   
     public function getList() //instancie une collection d'objets ports
